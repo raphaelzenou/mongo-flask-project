a42a4aa1bb6ddc8656e9612f8e67fd0cf507d550
Raph <raphael.p.zenou@gmail.com>
2019-10-15 22:22:54 +0100

    connection to mongoDB + test

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
 
 #mongo DB details:
-#mongo_pwd = os.environ.get('MONGOPWD')
-#mongo_url = 'mongodb+srv://mongodb_admin:' + mongo_pwd + '@cluster0-byamh.gcp.mongodb.net/test?retryWrites=true&w=majority'
+# Note that app.config["MONGO_DBNAME"] = 'mongo-flask-project' is now optional
+mongo_pwd = os.environ.get('MONGOPWD')
+mongo_url = 'mongodb+srv://mongodb_admin:' + mongo_pwd + '@cluster0-byamh.gcp.mongodb.net/test?retryWrites=true&w=majority'
 
+app.config["MONGO_URI"] = mongo_url
+
+#Creating a PyMongo instance
+mongo = PyMongo(app)
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-	return 'Bonjour Heroku'
+@app.route('/get_items_tracked')
+def get_items_tracked():
+	return render_template('items.html', items=mongo.db.items.find())
 
 if __name__=='__main__':
 	# Environment variables set in heroku
