ac878ffeb22488bbd6182d13e32530add7d24f52
Raph <raphael.p.zenou@gmail.com>
2019-10-15 18:33:45 +0100

    sorting out environment variables

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import os
 from flask import Flask
 
+#mongo DB details:
+#mongo_pwd = os.environ.get('MONGOPWD')
+#mongo_url = 'mongodb+srv://mongodb_admin:' + mongo_pwd + '@cluster0-byamh.gcp.mongodb.net/test?retryWrites=true&w=majority'
+
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,7 +13,7 @@ def hello():
 	return 'Bonjour World'
 
 if __name__=='__main__':
-	app.run()
-	# app.run(host=os.environ.get('IP'),
-	# 	port=int(os.environ.get('PORT')),
-	# 	debug=True)
+	# Environment variables set in heroku
+	app.run(host=os.environ.get('IP'),
+	port=int(os.environ.get('PORT')),
+	debug=True)
